5f3b324db051bac5b43bb5945af587e7d342b8cb
Francis Rocco <roccofrancis@gmail.com>
2016-11-29 16:23:51 -0500

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+      catbook.com/api/v1/cat
+      namespace :api do
+        namespace :v1 do
+          resources :users
+          resources :posts
+          resources :comments
+          resources :replies
+          resources :messages
+        end
+      end
+    end
