b4fa513cf929ece99a86319ae37ab63059deb5f3
Francis Rocco <roccofrancis@gmail.com>
2016-12-02 20:21:25 -0500

    add new post functionality

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,22 @@ module Api
     class PostsController < ApplicationController
 
       def index
-        render json: Post.all, include: { comments: [:replies] }
+        render json: Post.all.order('created_at DESC'), include: { comments: [:replies] }
+      end
+
+      def create
+        post = Post.new(post_params)
+        if post.save
+          render json: post
+        else
+          render json: post.errors, status: 500
+        end
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:title, :content)
       end
 
     end
