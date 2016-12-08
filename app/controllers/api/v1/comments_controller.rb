e3985fceeeb912e60dd17c3b1e67bfdcddd69ae2
Francis Rocco <roccofrancis@gmail.com>
2016-12-08 18:26:32 -0500

    add new comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,6 +6,21 @@ module Api
         render json: Comment.all
       end
 
+      def create
+        comment = Comment.new(comment_params)
+        if comment.save
+          render json: comment
+        else
+          render json: comment.errors, status: 500
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:content, :post_id)
+      end
+
     end
   end
 end
