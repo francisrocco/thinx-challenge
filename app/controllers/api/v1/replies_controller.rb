b7a3c0db5117631ba0f6d49bed98c81e9807b575
Francis Rocco <roccofrancis@gmail.com>
2016-12-12 14:52:22 -0500

    add new reply feature

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -6,6 +6,21 @@ module Api
         render json: Reply.all
       end
 
+      def create
+        reply = Reply.new(reply_params)
+        if reply.save
+          render json: reply
+        else
+          render json: reply.errors, status: 500
+        end
+      end
+
+      private
+
+      def reply_params
+        params.require(:reply).permit(:content, :comment_id)
+      end
+
     end
   end
 end
